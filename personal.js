1a13ffa3e433f066b2af9f22d67534eafc59fa9c
farfro <samideacon17@gmail.com>
2023-03-16 18:56:49 -0700

    added some text and touch ups

--- personal.js
+++ personal.js
@@ -1,28 +1,28 @@
-let burger = document.querySelector('.closed');
-let x = document.querySelector('.opened');
-let div = document.querySelector('.column4');
-let last = document.querySelector('.last');
-let a = document.querySelectorAll('.mynav');
+let burger = document.querySelector(".closed");
+let x = document.querySelector(".opened");
+let div = document.querySelector(".column4");
+let last = document.querySelector(".last");
+let a = document.querySelectorAll(".mynav");
 
-for(let i of a) {
-    i.addEventListener("click", () => {
-        div.style.display = 'none';
-        burger.style.display = 'inline';
-        x.style.display = 'none';
-        last.classList.remove('blur');
-    })
+for (let i of a) {
+  i.addEventListener("click", () => {
+    div.style.display = "none";
+    burger.style.display = "inline";
+    x.style.display = "none";
+    last.classList.remove("blur");
+  });
 }
 
 burger.addEventListener("click", () => {
-    div.style.display = 'block';
-    burger.style.display = 'none';
-    x.style.display = 'inline-block';
-    last.classList.add('blur');
-})
+  div.style.display = "block";
+  burger.style.display = "none";
+  x.style.display = "inline-block";
+  last.classList.add("blur");
+});
 
 x.addEventListener("click", () => {
-    div.style.display = 'none';
-    burger.style.display = 'inline';
-    x.style.display = 'none';
-    last.classList.remove('blur');
-})
+  div.style.display = "none";
+  burger.style.display = "inline";
+  x.style.display = "none";
+  last.classList.remove("blur");
+});
